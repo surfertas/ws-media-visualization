c1812eba09c9cf98d8fb8e890812c4a93cf297d6
surfertas <tasuku@gmail.com>
2017-05-08 20:51:04 +0900

    app.py

--- src/app.py
+++ src/app.py
@@ -3,23 +3,17 @@
 # author: Tasuku Miura
 
 import os
-from sqlite3 import dbapi2 as sqlite3
 from flask import Flask, g, request
 from views import view
 
 configpath = os.path.expanduser('~')
-configdb = "user.db"
-configschema = "schema.sql"
 MEDIA_BANK_PATH = "{}/media".format(configpath)
 
 
-def init_app(schema_uri, db_uri):
+def init_app():
     app = Flask(__name__)
-
     app.config.update(dict(
         MEDIAFOLDER=MEDIA_BANK_PATH,
-        SCHEMA=os.path.join(app.root_path, schema_uri),
-        DATABASE=os.path.join(app.root_path, db_uri),
         DEBUG=True
     ))
 
@@ -27,5 +21,5 @@ def init_app(schema_uri, db_uri):
     return app
 
 if __name__ == '__main__':
-    app = init_app(configschema, configdb)
+    app = init_app()
     app.run(host='0.0.0.0', port=5050)
